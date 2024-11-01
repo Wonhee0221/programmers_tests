32f438193dab22178d719424014833493f8eef08
WonHee Han <91041488+Wonhee0221@users.noreply.github.com>
2024-11-02 08:29:38 +0900

    [level 3] Title: 등굣길, Time: 3.72 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.py
+++ 프로그래머스/3/42898. 등굣길/등굣길.py
@@ -1,13 +1,13 @@
 def solution(m, n, puddles):
-    puddles = set((j, i) for i, j in puddles)  # 집합으로 변환
-    graph = [[0]*(m+1) for _ in range(n+1)]
-    graph[1][1] = 1
+    dp = [[0]*(m+1) for _ in range(n+1)]
+    dp[1][1] = 1
+    puddles = [(x,y) for y,x in puddles]
     for i in range(1,n+1):
-        for x in range(1,m+1):
-            if i == 1 and x == 1:
+        for j in range(1,m+1):
+            if i == 1 and j == 1:
                 continue
-            if (i,x) in puddles:
-                graph[i][x] = 0
+            elif (i,j) in puddles:
+                dp[i][j] = 0
             else:
-                graph[i][x] = (graph[i-1][x] + graph[i][x-1]) % 1000000007
-    return graph[n][m] 
+                dp[i][j] += (dp[i-1][j] + dp[i][j-1]) % 1000000007
+    return dp[n][m]
