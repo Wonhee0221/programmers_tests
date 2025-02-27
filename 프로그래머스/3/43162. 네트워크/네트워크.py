19aefe3277f83ddf3de7a924b204b39aecb110ed
WonHee Han <91041488+Wonhee0221@users.noreply.github.com>
2025-02-27 17:32:03 +0900

    [level 3] Title: 네트워크, Time: 0.69 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,21 +1,14 @@
-from collections import deque
-def BFS(n,computers, i, visited):
-    visited[i] = True
-    que = deque()
-    que.append(i)
-    while que:
-        i = que.popleft()
-        visited[i] = True
-        for x in range(n):
-            if x != i and computers[i][x] == 1:
-                if visited[x] == False:
-                    que.append(x)
-    
 def solution(n, computers):
-    visited = [False for _ in range(n)]
     answer = 0
-    for i in range(n):
-        if visited[i] == False:
+    visited = [False] * n
+    
+    def dfs(x):
+        visited[x] = True
+        for y in range(n):
+            if y != x and computers[x][y] == 1 and not visited[y]:
+                dfs(y)
+    for x in range(n):
+        if not visited[x]:
+            dfs(x)
             answer += 1
-            BFS(n,computers, i,visited)
     return answer
